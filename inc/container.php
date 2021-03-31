9ff76449eb7bb415f42b259db266b6a710bd6877
ranjan-apu <65912359+ranjan-apu@users.noreply.github.com>
2021-03-31 14:46:12 +0530

    Add files via upload

---
+++ inc/container.php
@@ -0,0 +1,26 @@
+
+</head>
+<body class="" style="background-image: url(https://www.nitk.ac.in/design-system/campus-images/mb3.jpg); background-repeat:no-repeat; background-size: cover;" >
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="https://iris.nitk.ac.in" class="navbar-brand">NIT-K</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="https://iris.nitk.ac.in">Home</a></li>
+           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	
